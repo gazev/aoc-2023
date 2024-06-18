9d534c3622f9d46ddadeaf7b5177965dd0b84986
Gonçalo Azevedo <goncalo.r.azevedo@tecnico.ulisboa.pt>
2024-06-18 23:08:19 +0100

    fix get_input script

--- get_input.sh
+++ get_input.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-source .env
+if [ $# -eq 0 ]; then
+    echo "Need day as first argument!"
+    exit
+fi
 
-if [ $# -eq 0 ]
-then
-    echo "Need day to be specified"
+if [[ ! $1 =~ ^[0-9]+$ ]] || [ ! $1 -ge 1 ] || [ ! "$1" -le 25 ]; then
+    echo "Number must an integer between 1 and 25!"
+    exit
 fi
 
 mkdir -p "day$1"
+
+source .env
 curl -sH "Cookie: session=$SESSION_TOKEN" https://adventofcode.com/2023/day/$1/input > ./day$1/input.txt
+
 echo "Input is now in day$1/input.txt ! :D"
